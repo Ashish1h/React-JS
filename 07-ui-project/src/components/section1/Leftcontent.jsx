5acb4abc1d0ee1c95f3d35d8b7f380980078cf27
Ashish1h <ashishydv196@gmail.com>
2026-02-08 01:27:42 +0530

    Fix typo in Leftcontent component description

--- 07-ui-project/src/components/section1/Leftcontent.jsx
+++ 07-ui-project/src/components/section1/Leftcontent.jsx
@@ -6,7 +6,7 @@ const Leftcontent = () => {
     <div className='h-full w-1/3 bg-white overflow-hidden'>
       <div className='p-6'>
         <div><h3 className='mb-7  text-4xl font-bold'>Prospective<br/> <span>customer</span> <br/> Segmentation</h3></div>
-        <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias voluptate deleniti modi, expedita hic autem explicabo, minus ipsum, ipsa qui accusamus? Dolorem sint in iste error debitis animi eos at.</p>
+        <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias voluptate deleniti modi, expedita hic autem explicabo, minus ipsum, ipsa qui accusamus? Dolorem sint in iste error debitis animi eos</p>
       </div>
       <div className='text-9xl'><i className='ri-arrow-right-up-line'><MoveUpRight /></i></div>
       <div></div>
